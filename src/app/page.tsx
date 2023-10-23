f09cbea262b886b449c4fd5427cc5942017b51a4
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2023-10-23 11:41:52 +0300

    Add Carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import Navbar from "@/components/Navbar"
+// import PeriodeCarousel from "@/components/PeriodeCarousel"
 
 export default function Home() {
     return (
         <main>
-            <section className="h-screen min-h-[535px] bg-[url('/images/hero.jpg')] bg-cover bg-top bg-no-repeat bg-black/30 bg-blend-multiply">
+            {/* Hero section start */}
+            <section className="h-screen min-h-[768px] bg-[url('/images/hero.jpg')] bg-cover bg-top bg-no-repeat bg-black/30 bg-blend-multiply">
                 <Navbar />
-                <h1 className="lg:text-9xl text-5xl whitespace-nowrap text-white font-extrabold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h1 className="whitespace-nowrap text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     PPI TURKI
                 </h1>
             </section>
+            {/* Hero section end */}
+
+            {/* Sejarah section start */}
+
+            {/* <PeriodeCarousel /> */}
+
+            {/* Sejarah section end */}
         </main>
     )
 }
---
+++ src/components/PeriodeCarousel.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import Image from "next/image"
+import { DragEventHandler } from "react"
+import AliceCarousel, { Responsive } from "react-alice-carousel"
+import "react-alice-carousel/lib/alice-carousel.css"
+
+const sejarahJson = [
+    { tahun: "2019/2020", img: "/images/sejarah_img.jpg" },
+    { tahun: "2020/2021", img: "/images/sejarah_img.jpg" },
+    { tahun: "2021/2022", img: "/images/sejarah_img.jpg" },
+    { tahun: "2022/2023", img: "/images/sejarah_img.jpg" },
+    { tahun: "2023/2024", img: "/images/sejarah_img.jpg" },
+]
+
+const handlerDragStart: DragEventHandler<HTMLDivElement> = (event) => event.preventDefault()
+
+const sejarahPeriode = sejarahJson.map((value, index) => (
+    <div key={index} onDragStart={handlerDragStart}>
+        <Image src={value.img} alt={value.tahun} width={380} height={380} className="rounded-xl" />
+        <div className="px-4 py-2 rounded-xl w-fit bg-[#B8A698]">{value.tahun}</div>
+    </div>
+))
+
+const CarouselResponsive: Responsive = {
+    0: {
+        items: 1,
+    },
+    1024: {
+        items: 3,
+        itemsFit: "contain",
+    },
+}
+
+export default function PeriodeCarousel() {
+    return (
+        <section className="pt-28 pb-24 lg:pt-36 lg:pb-32 bg-[#2F292D]">
+            <div className="container">
+                <h2 className="uppercase text-white text-center mb-28">
+                    SEJARAH PERIODE PPI TURKI
+                </h2>
+                <AliceCarousel
+                    infinite
+                    responsive={CarouselResponsive}
+                    disableDotsControls
+                    items={sejarahPeriode}
+                />
+            </div>
+        </section>
+    )
+}
